a4b1cb1e73acb76fddfab14a18948829d23d071c
Jason <jasnael94@gmail.com>
2025-01-31 11:15:02 +0100

    updating

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # scraping
+
+SCRAPING:
+tuto : https://www.youtube.com/watch?v=Y_NlDyUfVJ8&ab_channel=RomainKania
+
+-créer un dossier “scraping” et ouvrir avec VS
+- dans le dossier : “npm init” pour avoir le package.json
+- créer index.js
+- dans package.json rajouter dans “script” :
+       "start" : "node index.js",  = pour “node index.js” on peut “npm start”;pour démarrer projet
+-installer module puppeteer pour controller chromium :
+   npm install puppeteer --save
+
+-créer le fichier “index.js”
+
+
+Dans “ index.js ” :
+
+-importer la bibliothèque puppeteer:
+   const puppeteer = require('puppeteer');
+
+- créer une fonction pour scraper
+	(async () => {
+    const browser = await puppeteer.launch({headless:false}); //instancer le browser , option    "headless:false= avec interface grafique ”, “headless:true “ =sans interface grafique
+    const page = await browser.newPage(); // pour ouvrir une nouvelle page
+	wait page.goto('https://www.imdb.com/calendar/?ref_=hm_cs_sm')// goto=promesse , aller sur la page choisie
+})();
+
+
+le selector (utililiser inspect , console):
+- inspecter l’element (sur la page choisie)
+-sélectionner élément souhaiter et copier>sélecteur css ou copy>copy selector
+- dans console :
+    >> document.querySelector(‘coller sélecteur’)pour un élément et
+    >> document.querySelectorAll(‘coller sélecteur’)pour tous les éléments 
+
+Maintenant dans le fichier index.js rajouter le sélecteur :
+
+	(async () => {
+    const browser = await puppeteer.launch({headless:false}); 
+    const page = await browser.newPage(); 
+    await page.goto('url’);
+    document.querySelectorAll('’);
+
+})();
+
--- index.js
+++ index.js
@@ -1,23 +1,35 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    const browser = await puppeteer.launch({headless:true}); //instancier le browser , option "headless:false va démarrer Chromium"
-    const page = await browser.newPage();// pour ouvrir une nouvelle page
-    await page.goto('https://www.imdb.com/calendar/?ref_=hm_cs_sm');// goto=promesse , aller sur la page du url 
-    await page.waitForSelector('#__next > main > div > div.ipc-page-content-container.ipc-page-content-container--center > section > section > article:nth-child(2) > ul > li',{ timeout: 60000 });
-    const movies= await page.evaluate(() =>{        // methode pour effectuer le code dans la page elle même
-        let movies =[];
-        let elements = document.querySelectorAll('#__next > main > div > div.ipc-page-content-container.ipc-page-content-container--center > section > section > article:nth-child(2) > ul > li'); // selection tous les element de la liste selectionéé
-        for (element of elements){
-            movies.push({
-            img:element.querySelector('img.ipc-image').src ,
-            title:element.querySelector('#__next > main > div > div.ipc-page-content-container.ipc-page-content-container--center > section > section > article:nth-child(2) > ul > li:nth-child(1) > div.ipc-metadata-list-summary-item__c > div > a').text,
-            genre:element.querySelector('#__next > main > div > div.ipc-page-content-container.ipc-page-content-container--center > section > section > article:nth-child(2) > ul > li:nth-child(1) > div.ipc-metadata-list-summary-item__c > div > ul.ipc-inline-list.ipc-inline-list--show-dividers.ipc-inline-list--no-wrap.ipc-inline-list--inline.ipc-metadata-list-summary-item__tl.base > li:nth-child(1) > span').textContent,
-            actor:element.querySelector('#__next > main > div > div.ipc-page-content-container.ipc-page-content-container--center > section > section > article:nth-child(2) > ul > li:nth-child(1) > div.ipc-metadata-list-summary-item__c > div > ul.ipc-inline-list.ipc-inline-list--show-dividers.ipc-inline-list--no-wrap.ipc-inline-list--inline.ipc-metadata-list-summary-item__stl.base > li:nth-child(1) > span').textContent,
-            });
-        };
-        return movies ;
+    const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage();
+    
+    await page.goto('https://www.imdb.com/calendar/?ref_=hm_cs_sm');
+    
+    await page.waitForSelector('#__next > main > div > div.ipc-page-content-container> section > section > article:nth-child(2) > ul > li', {
+        timeout: 60000
     });
+    
+    const movies = await page.evaluate(() => {
+        let film = [];
+        let elements = document.querySelectorAll('#__next > main > div > div.ipc-page-content-container> section > section > article:nth-child(2) > ul > li');
+        console.log("🚀 ~ movies ~ elements:", elements)
+        
+        for (let element of elements) {
+            try {
+                movies.push({
+                    img: element.querySelector('img.ipc-image').src,
+                    title: element.querySelector('div.ipc-metadata-list-summary-item__c div > a')?.text,
+                    genre: element.querySelector('ul.ipc-metadata-list-summary-item__tl li:first-child span')?.textContent,
+                    actor: element.querySelector('ul.ipc-metadata-list-summary-item__stl li:first-child span')?.textContent
+                });
+            } catch (error) {
+                console.log('Erreur lors de l\'extraction d\'un film:', error);
+            }
+        }
+        return movies;
+    });
+    
     console.log(movies);
-    await browser.close();
-})()
+    // await browser.close();
+})();
